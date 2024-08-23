b3211fa75993099ebd5154833367f2c350c3ac1c
Kendrick Torrance <torraken000@warren.k12.in.us>
2024-08-23 13:52:53 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class booo {
+
+
+        public static void main(String[] args){
+        }
+
+
+        double v1 = 20.00;
+        double v2 = 80.00;
+        double v = (v1 + v2) * 100.00;
+        double r = v % 40.00;
+        boolean isNoRemainder = true;
+        System.out.println("isNoRemainder = " + noR);
+        if(!inoR) {
+                System.out.println("Got some remainder");
+        }
+                }
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+}
